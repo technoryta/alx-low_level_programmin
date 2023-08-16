7de93058db4eb0c9014afdc30cb98459c0a33828
technoryta <technoryta@gmail.com>
2023-08-15 19:16:37 -0700

    Script upload

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: nothing
+ */
+
+int main(void)
+{
+	int count = 2;
+
+	float x = 1;
+	float y = x + 1;
+	float z = x + y;
+
+	printf("%.0f, ", x);
+	printf("%.0f, ", y);
+	while (counter < 98)
+	{
+		count++;
+		printf("%.0f", z);
+		x = y;
+		y = z;
+		z = x + y;
+		if (count < 98)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
