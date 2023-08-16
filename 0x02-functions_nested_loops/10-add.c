df4dd2fbd6df126b45bc3cfbff42f51576e83732
technoryta <technoryta@gmail.com>
2023-08-15 17:34:12 -0700

    Script upload

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: returns the sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
