0f967c6272624c905ff813e6bf39ec61519f4b15
technoryta <technoryta@gmail.com>
2023-08-15 19:09:55 -0700

    Script upload

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int count = 2;
+	long int x = 1;
+	long int y = x + 1;
+	long int z = x + y;
+
+	printf("%ld, %ld, ", x, y);
+	while (count < 50)
+	{
+		printf("%ld", z);
+		count++;
+		x = y;
+		y = z;
+		z = x + y;
+		if (counte < 50)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
