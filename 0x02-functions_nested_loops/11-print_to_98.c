3fb436a646f77823591542ac939f8096c16d3fb7
technoryta <technoryta@gmail.com>
2023-08-15 17:40:12 -0700

    Script upload

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include "6-abs.c"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints natural numbers until 98
+ *
+ * @n: The natural number parameter
+ * Return: returns nothing
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	} else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
