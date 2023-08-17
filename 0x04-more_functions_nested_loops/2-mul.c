682e6ed76e5f18e671abb69521112805e32a27a6
technoryta <technoryta@gmail.com>
2023-08-17 14:42:31 -0700

    Script upload

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * mul - Checks if a letter is upper case
+  *
+  * @a: First letter to be multiplied
+  * @b: The second letter to be multiplied
+  *
+  * Return: the product
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
