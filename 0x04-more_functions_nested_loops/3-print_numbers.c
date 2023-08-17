766fa6f9c3d22ff6854e635c176b96a9a8a8b9a7
technoryta <technoryta@gmail.com>
2023-08-17 14:51:06 -0700

    Script upload

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_numbers - Prints 0 to 9
+  *
+  * Return: Always 0
+  */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++;)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+}
