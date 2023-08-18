4a695d62556172e24cb97f7bbfb011778d15bd62
technoryta <technoryta@gmail.com>
2023-08-18 05:59:12 -0700

    Script upload

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	unsigned int x = 2;
+	unsigned long n = 612852475143;
+
+	while (x != n)
+	{
+		if (n % x == 0)
+		{
+			n = n / x;
+		} else
+		{
+			x++;
+		}
+	}
+	printf("%lu\n", n);
+
+	return (0);
+}
