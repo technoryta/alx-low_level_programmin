c8378b10d133d96dbf1e29b0b0d548a6bf76025c
technoryta <technoryta@gmail.com>
2023-08-24 15:59:24 -0700

    Script upload

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_strncat - Concatenates two strings
+ *
+ *@dest: destination string
+ *@src: source string
+ *
+ *Return: returns destination string
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+		int i;
+		int j;
+
+		for (i = 0; dest[i] != '\0'; i++)
+		{
+		}
+		for (j = 0; j < n && src[j] != '\0'; j++)
+		{
+			dest[i + j] = src[j];
+		}
+		dest[i + j] = '\0';
+
+		return (dest);
+}
