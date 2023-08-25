cd5e0a288c2ae06d63a96066a129cf2649ce342c
technoryta <technoryta@gmail.com>
2023-08-24 17:14:15 -0700

    Script upload

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: returns the sign of difference
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int j = 0;
+
+	while (s1[j] - s2[j] == 0 && s1[j] != '\0')
+	{
+		j++;
+	}
+	return (s1[j] - s2[j]);
+}
