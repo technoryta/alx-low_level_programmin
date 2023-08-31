35ca93cd95ba585407219bebc0f2ba0b2885269e
technoryta <technoryta@gmail.com>
2023-08-30 17:07:20 -0700

    Script upload

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raises an integer to a power
+ *
+ * @x: base integer
+ * @y: exponent integer
+ *
+ * Return: -1 if y is lower than 0 else returns power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
