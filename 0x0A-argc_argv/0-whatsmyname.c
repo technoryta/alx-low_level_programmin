3dc20cb0c808584ea07462c3dea75772f30cf1f6
technoryta <technoryta@gmail.com>
2023-09-04 18:40:53 -0700

    Script upload

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -9,9 +8,9 @@
  * Return: returns 0
  */
 
-int main(int argc __attribute__((unused), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
 
