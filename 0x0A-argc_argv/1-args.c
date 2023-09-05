a7a69d544fcc3207d0f5493281a08ba224bc3816
technoryta <technoryta@gmail.com>
2023-09-04 18:50:42 -0700

    Script upload

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 0 for success
+ */
+
+int main(int argc, __attribute((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
