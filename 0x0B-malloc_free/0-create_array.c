01d69d520970414088a39900eac7440cbb2fd7b4
technoryta <technoryta@gmail.com>
2023-09-06 19:13:31 -0700

    Script upload

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+/**
+ * create_array - creates array of char
+ * @size: size of array
+ * @c: the array character for initialization
+ *
+ * Return: pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *p;
+
+	if (size == 0)
+		return (NULL);
+	p = malloc(sizeof(char) * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		p[i] = c;
+	return (p);
+}
