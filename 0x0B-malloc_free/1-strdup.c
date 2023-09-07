ae13e42d4ddc5f278e26f28bc5d984aefb68273a
technoryta <technoryta@gmail.com>
2023-09-06 19:48:03 -0700

    Script upload

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+/**
+ * _strdup - duplicates a string
+ * @str: string to duplicate
+ *
+ * Return: thestring duplicate
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int count = 0;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[count] != '\0')
+		count++;
+
+	p = malloc(sizeof(char) * count + 1);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < count; i++)
+		p[i] = str[i];
+
+	return (p);
+
+}
