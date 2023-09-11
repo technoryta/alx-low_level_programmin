bc98fe33519f978b11c2c3b8911e95bf7e5265f8
technoryta <technoryta@gmail.com>
2023-09-11 16:16:43 -0700

    Script upload

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - returns file name where compilsation occurs
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
