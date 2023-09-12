18a0635104552cdbcd91eb59036181978d1a36ce
technoryta <technoryta@gmail.com>
2023-09-11 17:48:45 -0700

    Script upload

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
  * init_dog - initializes a dog
@@ -15,4 +15,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->age = age;
 		d->owner = owner;
 	}
+	else
+		malloc(sizeof(struct dogtype));
 }
