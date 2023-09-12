2b26eb5dea3f95cd2b7b0640bb73f66c0f1c225e
technoryta <technoryta@gmail.com>
2023-09-11 17:35:36 -0700

    Script upload

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure with dog details
+ * @name: name of dog
+ * @owner: dog owner
+ * @age: age of dog
+ */
+typedef struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+} dogtype;
+
+#endif
