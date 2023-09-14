6fc8cac9f156294a13162f3023f26b8ce6ca6239
technoryta <technoryta@gmail.com>
2023-09-13 19:56:42 -0700

    Script upload

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+/**
+ * int_index - returns array index
+ * @array: array to search through
+ * @size: size of array
+ * @cmp: function that does the comparison
+ * Return: 1 or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		if cmp(array[i]) != 0
+			return (i);
+
+}
