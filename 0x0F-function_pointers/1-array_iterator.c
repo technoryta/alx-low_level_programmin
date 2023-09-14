6c9d8f7ecae88ead04c9b1f440bcc96934db5cf6
technoryta <technoryta@gmail.com>
2023-09-13 19:31:59 -0700

    Script upload

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_iterator - iterates through an array
+ * @array: array to iterate through
+ * @size: size of array
+ * @action: pointer to a function that takes action
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array != NULL && action != NULL)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
