e07ab28fccccb7b86f2fbc353cdd4aba4e4d4501
technoryta <technoryta@gmail.com>
2023-09-13 19:00:48 -0700

    Script upload

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints name
+ * @name: name to print
+ * @f: function that determines print type
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
