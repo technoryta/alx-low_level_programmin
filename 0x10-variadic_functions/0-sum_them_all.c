cf57961bfd4279e7c0a3ed8f52d11c3a3f6e8dc5
technoryta <technoryta@gmail.com>
2023-09-14 19:07:29 -0700

    Script upload

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,11 +9,13 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ap;
 	int i, sum;
 
-	sum = 0;
+	if (n == 0)
+		return (0);
+
 	va_start(ap, n);
-	if (n > 0)
-		for (i = 0; i < n; i++)
-			sum += va_arg(ap, int);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
 	va_end(ap);
 
 	return (sum);
