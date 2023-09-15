dcfe5ddf6eba10b722176de02a88e290d4a2ac21
technoryta <technoryta@gmail.com>
2023-09-14 19:19:29 -0700

    Script upload

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers passed
+ * @separator: a character that separates the numbers
+ * @n: number of integers passed
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i > 0 && separator != NULL)
+			printf("%s", separator);
+		printf("%d", va_arg(ap, int));
+	}
+	va_end(args);
+	printf("\n");
+}
