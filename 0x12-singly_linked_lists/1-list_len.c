c851af889b6712fe6fbd0988b2ab384f151a2d8f
technoryta <technoryta@gmail.com>
2023-09-21 19:45:53 -0700

    update script

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * list_len - prints number of elements
+ * @h: the head of the node
+ *
+ * Return: returns list size
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *current = h;
+	int length = 0;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		lenght++
+	}
+	return (length);
+}
+
