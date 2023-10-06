cdcaf0cbba598dcbd7b8db88f431b079a56cecd6
technoryta <technoryta@gmail.com>
2023-10-06 13:34:32 -0700

    updated script

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,8 +9,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, len = 0;
-	long int written;
+	int fd, written, len = 0;
 
 	if (filename == NULL)
 		return (-1);
