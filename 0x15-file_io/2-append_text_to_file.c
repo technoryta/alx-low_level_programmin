cfe824b6b403fe65c3c1c21529db9e6b495bbf65
technoryta <technoryta@gmail.com>
2023-10-06 13:23:16 -0700

    updated script

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * append_text_to_file - appends text to the end of a file
+ * @filename: name of file to read
+ * @text_content: text to append
+ * Return: 1 or -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, written, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_AAPEND);
+	if (fd < 0)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+		written = write(fd, text_content, len);
+		if (written < 0)
+			return (-1);
+	}
+	if (close(fd) < 0)
+		return (-1);
+	return (1);
+}
