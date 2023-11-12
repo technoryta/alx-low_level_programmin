af040f16ee06220fac1720b0757decac4addd6e7
technoryta <technoryta@gmail.com>
2023-11-11 18:49:13 -0800

    update code

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - Finds a node
+ * @head: The head of the list
+ * @index: The node position
+ * Return: node at index
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	int i;
+	dlistint_t *node;
+
+	node = *head;
+	for (i = 0; i <= index, i++)
+	{
+		if (node == NULL)
+			return (NULL);
+		node = node->next;
+	}
+	return (node);
+
+}
