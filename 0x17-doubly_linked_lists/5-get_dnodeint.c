69dacaa8ee2327d18170787ef8c940a49f3efab2
technoryta <technoryta@gmail.com>
2023-11-11 18:55:10 -0800

    update code

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *node;
 
 	node = head;
-	for (i = 0; i <= index; i++)
+	for (i = 0; i < index; i++)
 	{
 		if (node == NULL)
 			return (NULL);
