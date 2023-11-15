3b2c9fb0cec7a10b04623bb7f23d068b26eb0549
technoryta <technoryta@gmail.com>
2023-11-14 19:49:39 -0800

    update code

---
+++ 0x18-dynamic_libraries/calculate.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+/**
+ * add - adds two numbers
+ * @a: int
+ * @b: int
+ * Return: int
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * sub - adds two numbers
+ * @a: int
+ * @b: int
+ * Return: int
+*/
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * mul - adds two numbers
+ * @a: int
+ * @b: int
+ * Return: int
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * div - adds two numbers
+ * @a: int
+ * @b: int
+ * Return: int
+*/
+int div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * mod - adds two numbers
+ * @a: int
+ * @b: int
+ * Return: int
+*/
+int mod(int a, int b)
+{
+return (a % b);
+}
